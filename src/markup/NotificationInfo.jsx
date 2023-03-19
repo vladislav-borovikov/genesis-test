3112d285a9f83847b20f625d6a473e57cc3c3080
unknown <vladislavborovikov24@gmail.com>
2023-03-19 18:25:28 +0200

    fix

--- src/markup/NotificationInfo.jsx
+++ src/markup/NotificationInfo.jsx
@@ -3,15 +3,15 @@ import { toast } from "react-toastify";
 
 function NotificationInfo(message) {
     return toast.info(message, {
-position: "top-center",
-autoClose: 5000,
-hideProgressBar: false,
-closeOnClick: true,
-pauseOnHover: true,
-draggable: true,
-progress: undefined,
-theme: "light",
-})
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            })
 }
 
 export default NotificationInfo;
